92120ac3bb3928cb1cae3c05da12553d8e3ecae7
Rishik <rishikde09@gmail.com>
2021-07-26 14:56:56 -0700

    Removed redundant code; changed error message

--- comma.py
+++ comma.py
@@ -1,11 +1,12 @@
 def comma(s1):
     for f in range(len(s1)):
         if s1[f] not in "0123456789,":
-            raise Exception("")
+            raise Exception("Invalid input. Your input contains characters other than numbers and commas.")
         else:
-            for d in range(len(s1)):
-                if s1[d]=="," and s1[d+1]==",":
-                    raise Exception("Invalid input")
+            if s1[f]=="," and s1[f+1]==",":
+                raise Exception("Invalid input. Your input contains 2 or more consecutive commas.")
+            else:
+                return "The input is valid."
     
 while True:
     comma(input("Enter a string and i will validate it:"))
