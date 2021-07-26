aae57130b56a3c0fb52198f69a1f225f54fb9787
Rishik <rishikde09@gmail.com>
2021-07-26 09:42:09 -0700

    Corrected the mistake

--- comma.py
+++ comma.py
@@ -1,8 +1,11 @@
 def comma(s1):
     for f in range(len(s1)):
         if s1[f] not in "0123456789,":
-            print("NaN")
-            #return
+            raise Exception("")
         else:
-            print(1) 
-comma("06,,4")
+            for d in range(len(s1)):
+                if s1[d]=="," and s1[d+1]==",":
+                    raise Exception("Invalid input")
+    
+while True:
+    comma(input("Enter a string and i will validate it:"))
